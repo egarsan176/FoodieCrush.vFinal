266b8ec412d1ce5450688a8ff23b251c12d71177
egarsan176 <estefaniagarci@gmail.com>
2022-06-17 21:24:40 +0200

    cambiado nombre sin guiones

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/foodie-crush-final'));
+app.use(express.static(__dirname + '/dist/foodiecrushfinal'));
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/foodie-crush-final/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/foodiecrushfinal/index.html'));
 
 });
 app.listen(process.env.PORT || 8080);
