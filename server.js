d30f79dea069970631373f0f83d1de25f20331ca
egarsan176 <estefaniagarci@gmail.com>
2022-06-17 20:43:44 +0200

    actualizada version angular y modificados ficheros heroku

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/LaNube'));
+app.use(express.static(__dirname + '/dist/foodie-crush-final'));
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/LaNube/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/foodie-crush-final/index.html'));
 
 });
 app.listen(process.env.PORT || 8080);
